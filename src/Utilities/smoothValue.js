80e2f210e0507c19bbdb4a3b2c3db73bee1971f7
jeroennknol <jeroen-knol@live.nl>
2019-08-09 08:35:47 +0200

    Refactor SmoothValue

--- src/Utilities/smoothValue.js
+++ src/Utilities/smoothValue.js
@@ -1,37 +1,19 @@
-const smoothNegative = (animValue, curr, target, starting = null) => {
+const smoothValue = (animValue, curr, target, starting = null) => {
   const startingVal = starting || curr;
-
+  const endCondition = curr < target
+    ? (curr >= target) || curr > target - (target - startingVal) * 0.001 // smoothUpwards
+    : (curr <= target) || curr < target + (startingVal - target) * 0.001 // smoothDownwards
+     
   setTimeout(() => {
-    if ((curr <= target) || curr < target + (startingVal - target) * 0.01) {
+    if (endCondition) {
       animValue.set(target);
       return;
     };
+
     const next = (curr - target) * 0.85 + target;
-    animValue.set(next)
-    smoothPositive(animValue, next, target, startingVal);
+    animValue.set(next);
+    smoothValue(animValue, next, target, startingVal);
   }, 15);
-}
-
-const smoothPositive = (animValue, curr, target, starting = null) => {
-  const startingVal = starting || curr;
-
-  setTimeout(() => {
-    if ((curr >= target) || curr > target - (target - startingVal) * 0.01) {
-      animValue.set(target);
-      return;
-    };
-    const next = (curr - target) * 0.85 + target;
-    animValue.set(next)
-    smoothNegative(animValue, next, target, startingVal);
-  }, 15);
-}
-
-const smoothValue = (animValue, curr, target) => {
-  if (curr < target) {
-    smoothPositive(animValue, curr, target)
-  } else {
-    smoothNegative(animValue, curr, target)
-  }
 }
 
 export default smoothValue;
